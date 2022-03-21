02d5597af9dc9c84d1e38a1dafc53d03320de6c1
Gabriel Welvaert <gabewelvaert@gmail.com>
2022-03-21 10:21:55 -0400

    single file commit

--- lengthoflongestsubstring.py
+++ lengthoflongestsubstring.py
@@ -1,3 +1,4 @@
+#my solution
 def lengthOfLongestSubstring(s: str):
     lssl = start = stop = 0 # initialize longest sub-string length (w out repeating characters)
     while stop <= len(s)-1: #while stop has not "visited" last char
@@ -11,7 +12,36 @@ def lengthOfLongestSubstring(s: str):
 
     return lssl
 
+#32ms:
+# def lengthOfLongestSubstring(s: str) -> int:
+#     m = start = 0 #init m and start to 0
+#     d = {}
+#     for i, val in enumerate(s): # i = index of val
+#         print(d)
+#         if val in d:# and d[val] >= start:
+#             start = d[val] + 1
+#             print(start)
+#         elif i - start + 1 > m: # update return val if new larger one yeah yeah
+#             m = i - start + 1
+#         d[val] = i
+#     return m
 
+#50ms
+# def lengthOfLongestSubstring(s):
+#     ans = 0
+#     sub = ''
+#     for char in s:
+#         if char not in sub:
+#             sub += char
+#             ans = max(ans, len(sub))
+#         else:
+#             cut = sub.index(char)
+#             #print(f"cut={cut}")
+#             sub = sub[cut + 1:] + char
+#             #print(f"sub={sub}")
+#     return ans
+
+#testing:
 def tests():
     cases = {"abcabcbb":3, "bbbbb":1, "pwwkew":3, "":0," ":1}
     for x,y in cases.items():
