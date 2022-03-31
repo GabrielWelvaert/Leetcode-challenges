fc41fce76a0fcd45d47ee61f7bbcd7c5f572dab9
Gabriel Welvaert <gabewelvaert@gmail.com>
2022-03-30 21:48:35 -0400

    two sum init commit

---
+++ twoSum.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    d = {} #hash map value:index that will store "visisted" values of single pass soltuion:
+    for i, n in enumerate(nums):
+        diff= target - n
+        if diff in d:
+            return [d[diff],i] #return indicies of valid values
+        d[n] = i #add n:i to the hash map
+
+print(twoSum([2,1,5,3], 4))
+print(twoSum([2,7,11,15], 9))
