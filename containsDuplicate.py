f279a754175c4d04c181684b364f727d0035fa68
Gabriel Welvaert <gabewelvaert@gmail.com>
2022-03-31 17:27:56 -0400

    containsDuplicate commit

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,20 +1,8 @@
 def containsDuplicate(nums):
-    d = {} # key:value hash map
-    for i,n in enumerate(nums):
-        if n in d:
+    hashset = set()
+    for n in nums:
+        if n in hashset:
             return True
-        d[n] = i
+        hashset.add(n)
     return False
     #or return len(nums) != len(set(nums))
-
-
-def tests():
-    cases = {True:[1,2,3,4,5,6,7,8,9,1]}
-    for x,y in cases.items():
-        result = containsDuplicate(y)
-        if result == x:
-            print(f"Test passed with {y}: output was {result}")
-        else:
-            print(f"Test failed with {y}: output was {result} but should have been {y}")
-
-tests()
